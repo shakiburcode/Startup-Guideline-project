31896ef267aafc68616dbf53beeb170407f04fde
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-05-08 19:57:24 +0600

    Initial commit

---
+++ Shopping/src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label>Seekerrs Shop</label>
+            <i className='fa fa-envelope'></i>
+            <label> seekerrs@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Market Sales</label>
+            <label>Need Help?</label>
+            <span></span>
+            <label></label>
+            <span></span>
+            <label></label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
