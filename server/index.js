d720bf5807e425db8d3227f3bd1301a70613fd25
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2026-01-22 21:02:02 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
 
-
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
